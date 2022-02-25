b6efdbdd02bc61de587538aba3312bf447e9fa6c
jastreb2015 <82231229+jastreb2015@users.noreply.github.com>
2022-02-25 04:31:17 +0400

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
+/* 
 
-// You should implement your task here.
+function towelSort (matrix) {
+    arr = [];
+    arr2 = [];
+   
+    for (i = 0; i < matrix.length; i++) {
+        if (i%2 == 0) {
+       arr.push(matrix[i].sort((a, b) => a - b)); 
+        } else {
+        arr.push(matrix[i].sort((a, b) => b - a)) ; 
+        }
+    }
+    for (i = 0; i < arr.length; i++) {
+        for(j = 0; j < arr[i].length; j++) {
+            arr2.push(arr[i][j]);
+        }
+    }
+    return arr2;
+}
+ */
+/* console.log (towelSort([
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ],
+   ])); */
 
 module.exports = function towelSort (matrix) {
-  return [];
+    
+    if ( matrix == undefined || matrix == null) {
+        return  [];
+    }
+    arr = [];
+    arr2 = [];
+    for (i = 0; i < matrix.length; i++) {
+        if (i%2 == 0) {
+       arr.push(matrix[i].sort((a, b) => a - b)); 
+        } else {
+        arr.push(matrix[i].sort((a, b) => b - a)) ; 
+        }
+    }
+    for (i = 0; i < arr.length; i++) {
+        for(j = 0; j < arr[i].length; j++) {
+            arr2.push(arr[i][j]);
+        }
+    }
+    return arr2;
 }
+
+
+
